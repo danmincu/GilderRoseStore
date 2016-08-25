04c1445fae5ccfb9ab0cfb8d9630dbf6211afe16
Dan Mincu <danmincu@gmail.com>
2016-08-25 08:42:44 -0400

    Added test for multithreaded call of the IInventory.BuyItem

---
+++ GilderRoseStore.Tests/Models/InMemoryInventoryTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GilderRoseStore.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace GilderRoseStore.Tests.Models
+{
+    [TestClass]
+    public class InMemoryInventoryTest
+    {
+        [TestMethod]
+        public void TestBuyAnItemUntilTheStockIsEmpty()
+        {
+            var imi = new InMemoryInventory();
+            var firstLowStockItem = imi.Items.OrderBy(itm => itm.Quantity).FirstOrDefault();
+            Assert.IsNotNull(firstLowStockItem);
+            var initialQuanity = firstLowStockItem.Quantity;
+            for (int i = 0; i < initialQuanity ; i++)
+            {
+                Assert.IsTrue(imi.BuyItem(firstLowStockItem.Id));
+            }
+            Assert.IsFalse(imi.BuyItem(firstLowStockItem.Id));
+        }
+
+
+        [TestMethod]
+        public void TestAsyncCallForBuyRespectInventory()
+        {
+            var imi = new InMemoryInventory();
+            var mostNumerousStockItem = imi.Items.Where(itm => itm.Quantity < 20).OrderByDescending(itm => itm.Quantity).FirstOrDefault();
+            Assert.IsNotNull(mostNumerousStockItem);
+            var initialQuantity = mostNumerousStockItem.Quantity;
+            List<Task<bool>> tasks = new List<Task<bool>>();
+
+            for (int i = 0; i < 2 * initialQuantity; i++)
+            {
+                tasks.Add(Task.Run(() => { return imi.BuyItem(mostNumerousStockItem.Id); }));
+            }
+            Task.WhenAll(tasks).Wait();
+            Assert.AreEqual(initialQuantity, tasks.Count(t => t.Result));
+        }
+
+    }
+}
